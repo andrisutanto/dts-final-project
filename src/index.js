2787042d33d51d155696b681c82f97648eccd96e
Andri Sutanto <me@andrisutanto.com>
2022-08-03 22:47:45 +0700

    first init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './App.css';
 import App from './App';
+import UserContextProvider from './context/UserContext';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
